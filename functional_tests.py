06abca390c6d62a2c82b814e3ec6d8f23f6698fe
Daniel Taylor <daniel.taylor@alfresco.com>
2015-02-19 15:38:30 +0000

    ABD-739 - Better failing test

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Remote("http://docker.local.com:4444/wd/hub", webdriver.DesiredCapabilities.FIREFOX)
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Remote("http://docker.local.com:4444/wd/hub", webdriver.DesiredCapabilities.FIREFOX)
+		self.browser.implicitly_wait(3)
 
-browser.get('http://docker.local.com')
+	def tearDown(self):
+		self.browser.quit()
 
-assert 'License Generator' in browser.title, "Browser title was: " + browser.title
+	def test_site_loads(self):
+		# Alfrescan wants to generate a license, visits the URL
+		self.browser.get("http://docker.local.com")
 
-browser.quit()
+		# They notice the page title and header mention the license generator
+		self.assertIn("License Generator", self.browser.title)
+		self.fail("Finish the test!")
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
